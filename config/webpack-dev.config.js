ee48109990f6667ddde6d33728bfc93014705e45
Shadab Khan <shadab.2005@gmail.com>
2022-03-27 13:09:44 +0530

    chore: add svg loader

--- config/webpack-dev.config.js
+++ config/webpack-dev.config.js
@@ -44,6 +44,17 @@ module.exports = {
         },
       },
       {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
